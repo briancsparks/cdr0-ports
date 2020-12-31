f575950559853c35ed247d416378fa6d6b8cd9f2
Brian C Sparks <briancsparks@gmail.com>
2020-12-31 02:07:02 -0800

    Refactoring

--- polyrepo/cdr0-active-development.js
+++ polyrepo/cdr0-active-development.js
@@ -1,2 +1,4 @@
 
-module.exports = require('./get-local-polyrepo')(__dirname)('@cdr0/active-development');
+const polyPkgs = ['@cdr0/active-development', '@cdr0/grumpy'];
+
+module.exports = require('./get-local-polyrepo')(__dirname, {polyPkgs})('@cdr0/active-development')       || require('@cdr0/active-development');
