5a825ab72c3e045520cc4d26f14598d80b02721f
Brian C Sparks <briancsparks@gmail.com>
2020-12-31 05:34:51 -0800

    Refactoring

--- polyrepo/get-local-polyrepo.js
+++ polyrepo/get-local-polyrepo.js
@@ -16,10 +16,20 @@
 //
 /*
 
-const polyPkgs = ['@cdr0/sg', '@cdr0/grumpy'];
+// Choose one, and comment-out the other.
+const polyrepoLib = './get-local-polyrepo';     // Use this when changing polyrepo, itself
+const polyrepoLib = '@cdr0/polyrepo';           // Use this normally (the version in npm works for your needs)
 
-module.exports = require('./get-local-polyrepo')(__dirname, {polyPkgs})('@cdr0/sg')       || require('@cdr0/sg');
-module.exports = require('./get-local-polyrepo')(__dirname, {polyPkgs})('@cdr0/grumpy')   || require('@cdr0/grumpy');
+// Build the prequire (poly-require) function
+const polyrepoRequire   = require(polyrepoLib)(__dirname);
+
+// The function that will get called, whose results will be the package requested.
+const prequire  = function (name) {
+  return polyrepoRequire(name) || require(name);
+};
+
+module.exports = prequire('@cdr0/sg');
+
 */
 
 // (2) - require the above file, instead of the plain package in whatever files you need
